fd0ec62b9b349c7b9f395921f79b6aeb9597647f
Greeddk <suby0475@gmail.com>
2024-10-22 10:19:25 +0900

    Refactor Access Modifier

--- Sources/SimpleTooltip/View/TooltipModifierContainer.swift
+++ Sources/SimpleTooltip/View/TooltipModifierContainer.swift
@@ -25,31 +25,31 @@ public struct TooltipModifierContainer<Content: View>: View {
         content.modifier(TooltipModifier(text: text, isShowing: isShowing, options: options))
     }
     
-    func tooltipBackgroundColor(_ color: Color) -> TooltipModifierContainer {
+    public func tooltipBackgroundColor(_ color: Color) -> TooltipModifierContainer {
         var newContainer = self
         newContainer.options.backgroundColor = color
         return newContainer
     }
     
-    func tooltipTextColor(_ color: Color) -> TooltipModifierContainer {
+    public func tooltipTextColor(_ color: Color) -> TooltipModifierContainer {
         var newContainer = self
         newContainer.options.textColor = color
         return newContainer
     }
     
-    func tooltipAlignment(_ position: TrianglePosition) -> TooltipModifierContainer {
+    public func tooltipAlignment(_ position: TrianglePosition) -> TooltipModifierContainer {
         var newContainer = self
         newContainer.options.position = position
         return newContainer
     }
     
-    func tooltipOffset(_ offset: CGSize) -> TooltipModifierContainer {
+    public func tooltipOffset(_ offset: CGSize) -> TooltipModifierContainer {
         var newContainer = self
         newContainer.options.offset = offset
         return newContainer
     }
     
-    func tooltipZIndex(_ zIndex: Double) -> TooltipModifierContainer {
+    public func tooltipZIndex(_ zIndex: Double) -> TooltipModifierContainer {
         var newContainer = self
         newContainer.options.zIndex = zIndex
         return newContainer
